fc383ce73e58e24824bc734895f117bf22e38705
Dmitry <dima12_yarrin@mail.ru>
2024-03-06 02:06:49 +0300

    fix

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,5 +1,5 @@
 from utils import arrs
-
+from utils.arrs import get, my_slice
 
 def test_get():
     assert arrs.get([1, 2, 3], 1, "test") == 2
@@ -9,3 +9,23 @@ def test_get():
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+
+def test_get_existing_index():
+    array = [1, 2, 3, 4, 5]
+    assert get(array, 2) == 3
+
+def test_get_non_existing_index():
+    array = [1, 2, 3]
+    assert get(array, 5, 'default') == 'default'
+
+def test_my_slice_full_range():
+    coll = [1, 2, 3, 4, 5]
+    assert my_slice(coll) == coll
+
+def test_my_slice_with_start():
+    coll = [1, 2, 3, 4, 5]
+    assert my_slice(coll, 1) == [2, 3, 4, 5]
+
+def test_my_slice_with_end():
+    coll = [1, 2, 3, 4, 5]
+    assert my_slice(coll, end=3) == [1, 2, 3]
