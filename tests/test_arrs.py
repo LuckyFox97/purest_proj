b6e15ec2ca64294c6f8414f37428028c3ea83081
Dmitry <dima12_yarrin@mail.ru>
2024-03-06 02:26:00 +0300

    add tests

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,31 +1,30 @@
 from utils import arrs
 from utils.arrs import get, my_slice
 
-def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 2
-    assert arrs.get([], 0, "test") == "test"
+def test_get_with_valid_index():
+    assert get([1, 2, 3], 1) == 2
 
 
-def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+def test_my_slice_with_none_start():
+    assert my_slice([1, 2, 3, 4, 5], None, 3) == [1, 2, 3]
 
-def test_get_existing_index():
-    array = [1, 2, 3, 4, 5]
-    assert get(array, 2) == 3
+def test_get_with_out_of_range_index():
+    assert get([1, 2, 3], 10) is None
 
-def test_get_non_existing_index():
-    array = [1, 2, 3]
-    assert get(array, 5, 'default') == 'default'
+def test_my_slice_with_valid_range():
+    assert my_slice([1, 2, 3, 4, 5], 1, 4) == [2, 3, 4]
 
-def test_my_slice_full_range():
-    coll = [1, 2, 3, 4, 5]
-    assert my_slice(coll) == coll
+def test_my_slice_with_negative_start():
+    assert my_slice([1, 2, 3, 4, 5], -3) == [3, 4, 5]
 
-def test_my_slice_with_start():
-    coll = [1, 2, 3, 4, 5]
-    assert my_slice(coll, 1) == [2, 3, 4, 5]
+def test_my_slice_with_negative_end():
+    assert my_slice([1, 2, 3, 4, 5], 1, -1) == [2, 3, 4]
 
-def test_my_slice_with_end():
-    coll = [1, 2, 3, 4, 5]
-    assert my_slice(coll, end=3) == [1, 2, 3]
+def test_my_slice_with_out_of_range_start():
+    assert my_slice([1, 2, 3, 4, 5], -10) == [1, 2, 3, 4, 5]
+
+def test_my_slice_with_out_of_range_end():
+    assert my_slice([1, 2, 3, 4, 5], 10) == []
+
+def test_my_slice_with_empty_list():
+    assert my_slice([]) == []
